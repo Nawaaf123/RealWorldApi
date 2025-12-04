6ffd98a6aee6cb2d5fea18ed134129ce82c46d3c
Nawaaf123 <nawaafmohd22@gmail.com>
2025-12-04 00:00:48 -0600

    Initial commit - RealWorld API

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Enable Swagger ALWAYS (DevOps apps need it in Docker)
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// ❌ IMPORTANT: Disable HTTPS redirect (Docker has no HTTPS cert)
+ // app.UseHttpsRedirection();   // <-- REMOVE OR COMMENT THIS
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/version", () =>
+{
+    return new { version = "v1", time = DateTime.Now.ToString() };
+});
+
+app.Run();
